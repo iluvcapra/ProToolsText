0ece250cfc35ea2c6ef83dc2082ae5321fe27b1f
Jamie Hardt <jamiehardt@me.com>
2017-12-17 13:42:12 -0800

    Added PKit target

---
+++ PKit/PKit.h
@@ -0,0 +1,18 @@
+//
+//  PKit.h
+//  PKit
+//
+//  Created by Jamie Hardt on 12/17/17.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for PKit.
+FOUNDATION_EXPORT double PKitVersionNumber;
+
+//! Project version string for PKit.
+FOUNDATION_EXPORT const unsigned char PKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PKit/PublicHeader.h>
+
+
