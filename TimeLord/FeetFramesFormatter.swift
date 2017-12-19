ebf817ebb92f6fdb9c1be07d67643aba317de891
Jamie Hardt <jamiehardt@me.com>
2017-12-18 22:29:36 -0800

    Added some time arithmetic goodies

---
+++ TimeLord/FeetFramesFormatter.swift
@@ -0,0 +1,123 @@
+//
+//  FeetFramesFormatter.swift
+//  TimeLord
+//
+//  Created by Jamie Hardt on 12/17/17.
+//
+
+import Cocoa
+import CoreMedia
+
+/*
+ #include <stdlib.h>
+ 
+ typedef struct _FFResult {
+ int feet;
+ unsigned frame;
+ unsigned thisFootStartsOnPerf;
+ } FFResult;
+ 
+ FFResult FeetFrames(int frameCount, unsigned perfsPerFrame, unsigned perfsPerFoot) {
+ 
+ int   totalPerfs   = frameCount * perfsPerFrame;
+ div_t feetResult   = div(totalPerfs     ,  perfsPerFoot);
+ div_t framesResult = div(feetResult.rem ,  perfsPerFrame);
+ 
+ FFResult retVal = {
+ .feet = feetResult.quot,
+ .frame = framesResult.quot,
+ .thisFootStartsOnPerf = (perfsPerFrame - framesResult.rem) % perfsPerFrame
+ };
+ 
+ return retVal;
+ }
+ 
+ int main(int argc, const char **argv) {
+ 
+ printf("35mm 4perf  35mm 3perf  16mm\n");
+ int i;
+ for (i = 0; i < 128; i++) {
+ FFResult r = FeetFrames(i,4,64);
+ printf("%i+%02i.%i      ",r.feet,r.frame,r.thisFootStartsOnPerf);
+ 
+ r = FeetFrames(i,3,64);
+ printf("%i+%02i.%i      ",r.feet,r.frame,r.thisFootStartsOnPerf);
+ 
+ r = FeetFrames(i,1,40);
+ printf("%i+%02i.%i\n",r.feet,r.frame,r.thisFootStartsOnPerf);
+ }
+ return 0;
+ }
+ 
+ */
+
+struct FeetFrames {
+    var feet : Int
+    var frame : Int
+    var footFraming : Int
+    
+    static func from(frameCount : Int, perfsPerFrame : Int, perfsPerFoot : Int) -> FeetFrames {
+        
+        let totalPerfs = frameCount * perfsPerFrame
+        let feetResult = totalPerfs.quotientAndRemainder(dividingBy: perfsPerFoot)
+        let framesResults = feetResult.remainder.quotientAndRemainder(dividingBy: perfsPerFrame)
+        
+        return FeetFrames(feet: feetResult.quotient,
+                          frame: framesResults.quotient,
+                          footFraming: (perfsPerFrame - framesResults.remainder) % perfsPerFrame )
+    }
+}
+
+
+extension CMTime {
+    func divideWithRemainder(by time : CMTime) -> (quotient: Int, remainder: CMTime) {
+        if time == kCMTimeZero {
+            return (quotient: kCMTimeIndefinite, remainder : kCMTimeZero)
+        }
+        
+        if time.isPositiveInfinity || time.isNegativeInfinity {
+            return (quotient: 0, remainder : kCMTimeZero)
+        }
+        
+        let conv = self.convertScale(time.timescale, method: .roundAwayFromZero)
+        let result = conv.value.quotientAndRemainder(dividingBy: time.value)
+        
+        return (quotient : result.quotient,
+                remainder: CMTime(value: result.remainder, timescale: conv.timescale) )
+    }
+}
+
+class FeetFramesFormatter: Formatter {
+    
+    enum Rate {
+        case Frame23976
+        case Frame24
+    }
+    
+    var rate : Rate = .Frame24
+    
+    var fractionalFrames : Bool = false
+    
+    override func string(for obj: Any?) -> String? {
+        guard let time = obj as? CMTime else {
+            return nil
+        }
+        
+        if !time.isValid {
+            return "(Invalid)"
+        } else if time.isIndefinite {
+            return "(Indefinite)"
+        } else if time.isPositiveInfinity {
+            return "(+∞)"
+        } else if time.isNegativeInfinity {
+            return "(-∞)"
+        }
+        
+    }
+    
+    override func getObjectValue(_ obj: AutoreleasingUnsafeMutablePointer<AnyObject?>?,
+                                 for string: String,
+                                 errorDescription error: AutoreleasingUnsafeMutablePointer<NSString?>?) -> Bool {
+
+    }
+}
