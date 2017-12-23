eca8feff06f3eca867d1c59734be5f5cda015dde
Jamie Hardt <jamiehardt@me.com>
2017-12-23 12:54:31 -0800

    Ptext Convert app target

---
+++ PText Convert/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  PText Convert
+//
+//  Created by Jamie Hardt on 12/23/17.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
