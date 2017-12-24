3d7262838f82b44081a561c5dcfe319557dc8b5c
Jamie Hardt <jamiehardt@me.com>
2017-12-23 21:34:53 -0800

    Tage parsing implementation

--- PText Convert/CSVConversionEngine.swift
+++ PText Convert/CSVConversionEngine.swift
@@ -8,6 +8,166 @@
 import Cocoa
 import PKit
 
+class TagParser {
+    enum Token {
+        case Begin
+        case Whitespace
+        case Dollar
+        case LeftBrace
+        case RightBrace
+        case LeftSquareBracket
+        case RightSquareBracket
+        case Equals
+        case Word
+    }
+    
+    struct TagParserError : Error {
+        var at : Int
+    }
+    
+    var scanner :Scanner
+    var thisToken : Token = .Begin
+    var thisWord : String?
+    
+    var name = ""
+    var dict = [String:String]()
+    
+    func nextToken() {
+        let exclusion = CharacterSet(charactersIn: "{}[]$=").union(CharacterSet.whitespaces)
+        var buffer : NSString? = nil
+        if scanner.scanCharacters(from: .whitespaces, into: nil) {
+            thisToken = .Whitespace
+            
+        } else if scanner.scanString("$", into: nil) {
+            thisToken = .Dollar
+        } else if scanner.scanString("{", into: nil) {
+            thisToken = .LeftBrace
+        } else if scanner.scanString("}", into: nil) {
+            thisToken = .RightBrace
+        } else if scanner.scanString("[", into: nil) {
+            thisToken = .LeftSquareBracket
+        } else if scanner.scanString("]", into: nil) {
+            thisToken = .RightSquareBracket
+        } else if scanner.scanString("=", into: nil) {
+            thisToken = .Equals
+        } else if scanner.scanUpToCharacters(from: exclusion, into: &buffer) {
+            thisToken = .Word
+            thisWord = buffer! as String
+        } else {
+            return
+        }
+    }
+    
+    func accept(token : Token) -> Bool {
+        if token == thisToken {
+            nextToken()
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func expect(token : Token) throws {
+        if !accept(token: token) {
+            throw TagParserError(at: scanner.scanLocation)
+        }
+    }
+    
+    func nameDecl() {
+        while true {
+            if accept(token: .Word) {
+                name += thisWord ?? ""
+            } else if accept(token: .Whitespace) {
+                name += " "
+            } else {
+                break
+            }
+        }
+    }
+    
+    func dollarDecl() throws {
+        let key : String
+        let val : String
+        try expect(token: .Word)
+        key = thisWord!
+        try expect(token: .Equals)
+        try expect(token: .Word)
+        val = thisWord!
+        dict[key] = val
+    }
+    
+    func braceDecl() throws {
+        let key : String
+        var val : String
+        try expect(token: .Word)
+        key = thisWord!
+        try expect(token: .Equals)
+        val = ""
+        while true {
+            if accept(token: .Word) {
+                val.append(thisWord!)
+            } else if accept(token: .Whitespace) {
+                val.append(" ")
+            } else {
+                break
+            }
+        }
+        try expect(token: .RightBrace)
+        dict[key] = val
+    }
+    
+    func bracketDecl() throws {
+        let key : String
+        try expect(token: .Word)
+        key = thisWord!
+        try expect(token: .RightSquareBracket)
+        dict[key] = key
+    }
+    
+    func fields() {
+        while !scanner.isAtEnd {
+            if accept(token: .Dollar) {
+                try? dollarDecl()
+            } else if accept(token: .LeftBrace) {
+                try? braceDecl()
+            } else if accept(token: .LeftSquareBracket) {
+                try? bracketDecl()
+            } else {
+                nextToken()
+            }
+        }
+    }
+    
+    func taggedString() {
+        nameDecl()
+        fields()
+    }
+    
+    func parse() -> [String:String] {
+        name = ""
+        try! expect(token: .Begin)
+        taggedString()
+        dict["_name"] = name.trimmingCharacters(in: .whitespaces)
+        return dict
+    }
+    
+    init(string s: String) {
+        scanner = Scanner(string: s)
+        scanner.charactersToBeSkipped = nil
+    }
+    
+}
+
+class TagInterpreter {
+    
+    func interpretClips(inTracks: [PTEntityParser.TrackEntity],
+                        applyMarkers: [PTEntityParser.MarkerEntity],
+                        applySession: PTEntityParser.SessionEntity) {
+       // let sessionFields = extractFields(from: applySession.rawTitle)
+        
+    }
+}
+
 class CSVConversionEngine: NSObject {
     
     var outputRecords : [[String:String]] = []
@@ -20,5 +180,7 @@ class CSVConversionEngine: NSObject {
         
         let fileData = try Data(contentsOf: fileURL)
         try textParser.parse(data: fileData, encoding: encoding.rawValue)
+        
+        
     }
 }
--- PText ConvertTests/PText_ConvertTests.swift
+++ PText ConvertTests/PText_ConvertTests.swift
@@ -10,26 +10,49 @@ import XCTest
 
 class PText_ConvertTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let tp = TagParser(string: "this is a test [A] [B] {Sc=23 Int. House}")
+        let result = tp.parse()
+        
+        XCTAssertTrue(result.keys.contains("A"))
+        XCTAssertTrue(result.keys.contains("B"))
+        XCTAssertTrue(result.keys.contains("_name"))
+        XCTAssertTrue(result.keys.contains("Sc"))
+        
+        XCTAssertEqual(result["A"], "A")
+        XCTAssertEqual(result["B"], "B")
+        XCTAssertEqual(result["_name"], "this is a test")
+        XCTAssertEqual(result["Sc"], "23 Int. House")
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testDollar() {
+        let tp = TagParser(string: "this is some text $V=100 $X=HELLO $QN=1001")
+        let result = tp.parse()
+        
+        XCTAssertTrue(result.keys.contains("_name"))
+        XCTAssertTrue(result.keys.contains("V"))
+        XCTAssertTrue(result.keys.contains("X"))
+        XCTAssertTrue(result.keys.contains("QN"))
+        
+        XCTAssertEqual(result["_name"], "this is some text")
+        XCTAssertEqual(result["V"], "100")
+        XCTAssertEqual(result["X"], "HELLO")
+        XCTAssertEqual(result["QN"], "1001")
     }
     
+    func testChars() {
+        let tp = TagParser(string: "\"This is a line– and some more after a pause...\" (sotto) [TBW] [OMIT] {Char=Bill Wilson}")
+        let result = tp.parse()
+        
+        XCTAssertTrue(result.keys.contains("_name"))
+        XCTAssertTrue(result.keys.contains("TBW"))
+        XCTAssertTrue(result.keys.contains("OMIT"))
+        XCTAssertTrue(result.keys.contains("Char"))
+        
+        XCTAssertEqual(result["_name"], "\"This is a line– and some more after a pause...\" (sotto)")
+        XCTAssertEqual(result["TBW"], "TBW")
+        XCTAssertEqual(result["OMIT"], "OMIT")
+        XCTAssertEqual(result["Char"], "Bill Wilson")
+    }
+
 }
