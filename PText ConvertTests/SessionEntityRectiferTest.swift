10e7ebd7aa0f3cc970450847f6c4f01f557df79e
Jamie Hardt <jamiehardt@me.com>
2017-12-24 13:29:14 -0800

    Restructured test slightly to lessen duplication

--- PText ConvertTests/SessionEntityRectiferTest.swift
+++ PText ConvertTests/SessionEntityRectiferTest.swift
@@ -17,40 +17,14 @@ class RectifierTestDelegate : SessionEntityRectifierDelegate {
 }
 
 class SessionEntityRectiferTest: XCTestCase {
-
-    let session = PTEntityParser.SessionEntity.init(rawTitle: "Test Session")
     
-    func testBasicClips() {
-        let testClips = [
-            PTEntityParser.ClipEntity(rawName: "Test 1",
-                                      eventNumber: 1, rawStart: "01:00:00:00", rawFinish: "01:00:01:00",
-                                      rawDuration: "00:00:01:00", muted: false),
-            PTEntityParser.ClipEntity(rawName: "Test 2",
-                                      eventNumber: 2, rawStart: "01:00:03:10", rawFinish: "01:00:04:10",
-                                      rawDuration: "00:00:01:00", muted: false),
-        ]
-        
-        let testTrack = PTEntityParser.TrackEntity(rawTitle: "Track 1", rawComment: "This is a track", clips: testClips)
-        
-        let r = SessionEntityRectifier(tracks: [testTrack], markers: [], session: session)
-        let d = RectifierTestDelegate()
-        r.delegate = d
-        
-        r.interpetRecords()
-        
-        XCTAssertTrue(d.records.count == 2)
-        XCTAssertEqual(d.records[0]["RawClipName"],"Test 1")
-        XCTAssertEqual(d.records[0]["Start"],"01:00:00:00")
-        XCTAssertEqual(d.records[0]["TrackName"],"Track 1")
-        XCTAssertEqual(d.records[1]["TrackName"],"Track 1")
-        XCTAssertEqual(d.records[1]["RawTrackName"],"Track 1")
-        XCTAssertEqual(d.records[1]["EventNumber"],"2")
-    }
+    var session : PTEntityParser.SessionEntity?
+    var testTrack : PTEntityParser.TrackEntity?
     
-    /*
-     This tests that fields set on track names copy to clips, but that fields on clips prevail.
-     */
-    func testTaggedClips() {
+    override func setUp() {
+        
+        session = PTEntityParser.SessionEntity.init(rawTitle: "Test Session")
+        
         let testClips = [
             PTEntityParser.ClipEntity(rawName: "Test 1 $A=1 {B=Hello}",
                                       eventNumber: 1, rawStart: "01:00:00:00", rawFinish: "01:00:01:00",
@@ -58,11 +32,34 @@ class SessionEntityRectiferTest: XCTestCase {
             PTEntityParser.ClipEntity(rawName: "Test 2 $A=2",
                                       eventNumber: 2, rawStart: "01:00:03:10", rawFinish: "01:00:04:10",
                                       rawDuration: "00:00:01:00", muted: false),
-            ]
+            ] 
         
-        let testTrack = PTEntityParser.TrackEntity(rawTitle: "Track 1 [D]", rawComment: "This is a track {B=Goodbye} {C=Z1}", clips: testClips)
+        testTrack = PTEntityParser.TrackEntity(rawTitle: "Track 1 [D]", rawComment: "This is a track {B=Goodbye} {C=Z1}", clips: testClips)
+    }
+    
+    func testBasicClips() {
+
+        let r = SessionEntityRectifier(tracks: [testTrack!], markers: [], session: session!)
+        let d = RectifierTestDelegate()
+        r.delegate = d
         
-        let r = SessionEntityRectifier(tracks: [testTrack], markers: [], session: session)
+        r.interpetRecords()
+        
+        XCTAssertTrue(d.records.count == 2)
+        XCTAssertEqual(d.records[0]["ClipName"],"Test 1")
+        XCTAssertEqual(d.records[0]["Start"],"01:00:00:00")
+        XCTAssertEqual(d.records[0]["TrackName"],"Track 1")
+        XCTAssertEqual(d.records[1]["TrackName"],"Track 1")
+        XCTAssertEqual(d.records[1]["TrackName"],"Track 1")
+        XCTAssertEqual(d.records[1]["EventNumber"],"2")
+    }
+    
+    /*
+     This tests that fields set on track names copy to clips, but that fields on clips prevail.
+     */
+    func testTaggedClips() {
+
+        let r = SessionEntityRectifier(tracks: [testTrack!], markers: [], session: session!)
         let d = RectifierTestDelegate()
         r.delegate = d
         
@@ -78,6 +75,15 @@ class SessionEntityRectiferTest: XCTestCase {
         XCTAssertEqual(d.records[1]["B"], "Goodbye")
         XCTAssertEqual(d.records[1]["C"], "Z1")
         XCTAssertEqual(d.records[1]["D"], "D")
+    }
+    
+    func testSessionTags() {
+
+        let r = SessionEntityRectifier(tracks: [testTrack!], markers: [], session: session!)
+        let d = RectifierTestDelegate()
+        r.delegate = d
+        
+        r.interpetRecords()
     }
 
 }
