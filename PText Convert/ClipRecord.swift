e8f88d7da83935074c9caf1870cd8963198798c7
Jamie Hardt <jamiehardt@me.com>
2018-01-14 15:52:54 -0800

    ClipRecord implementation

--- PText Convert/ClipRecord.swift
+++ PText Convert/ClipRecord.swift
@@ -6,16 +6,77 @@
 //
 
 import Foundation
+import PKit
 
-struct ClipRecord {
-    var sessionName : String
-    var trackName : String
-    var trackComment : String
-    var eventNumber : Int
-    var clipName : String
-    var start : String
-    var finish : String
-    var duration : String
-    var muted : Bool
-    var userData : [String:String]
+public struct ClipRecord {
+    public var sessionName : String
+    public var trackName : String
+    public var trackComment : String
+    public var eventNumber : Int
+    public var clipName : String
+    public var start : String
+    public var finish : String
+    public var duration : String
+    public var muted : Bool
+    public var userData : [String:String]
+    
+    public static func from(clip : PTEntityParser.ClipEntity,
+                     track: PTEntityParser.TrackEntity,
+                     session : PTEntityParser.SessionEntity ) -> ClipRecord {
+        return ClipRecord(sessionName: session.rawTitle,
+                          trackName: track.rawTitle,
+                          trackComment: track.rawComment,
+                          eventNumber: clip.eventNumber,
+                          clipName: clip.rawName,
+                          start: clip.rawStart, finish:
+                            clip.rawFinish,
+                          duration: clip.rawDuration,
+                          muted: clip.muted, userData: [:])
+    }
+    
+    public mutating func applyFieldsCanonically() {
+        applyClipNameFields()
+        applyTrackCommentFields()
+        applyTrackNameFields()
+        applySessionNameFields()
+    }
+    
+    /**
+     Applies fields in `sessionName` to `userData`. The session name is
+     then set to the untagged prefixing portion of `sessionName`.
+     */
+    mutating func applySessionNameFields() {
+        let result = TagParser(string: sessionName).parse()
+        sessionName = result.text
+        userData = userData.mergeKeepCurrent(result.fields)
+    }
+    
+    mutating func applyTrackCommentFields() {
+        let result = TagParser(string: trackComment).parse()
+        trackComment = result.text
+        userData = userData.mergeKeepCurrent(result.fields)
+    }
+    
+    mutating func applyTrackNameFields() {
+        let result = TagParser(string: trackName).parse()
+        trackName = result.text
+        userData = userData.mergeKeepCurrent(result.fields)
+    }
+    
+    /**
+     Applies fields in clip name to `userData`. The clip name is
+     then set to the unparsed prefixing portion of `clipName`.
+     */
+    mutating func applyClipNameFields() {
+        let p = TagParser(string: clipName)
+        let result = p.parse()
+        applyUserData(result.fields)
+        clipName = result.text
+    }
+    
+    /// merges the given dictionary with `userData`, keeping the
+    /// values of exisiting keys where they exist.
+    public mutating func applyToUserData(_ dict : [String:String]) {
+        userData = userData.mergeKeepCurrent(dict)
+    }
 }
