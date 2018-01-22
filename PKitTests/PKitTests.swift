426ac93865e048e73f3400109b977580d76bd819
Jamie Hardt <jamiehardt@me.com>
2018-01-21 20:40:13 -0800

    Removed dead code

--- PKitTests/PKitTests.swift
+++ PKitTests/PKitTests.swift
@@ -240,11 +240,5 @@ class PKitTests: XCTestCase {
         XCTAssertEqual(track5states.count, 1)
         XCTAssertTrue(track5states.contains("Hidden"))
     }
-//    func testPerformanceExample() {
-//        // This is an example of a performance test case.
-//        self.measure {
-//            // Put the code you want to measure the time of here.
-//        }
-//    }
     
 }
