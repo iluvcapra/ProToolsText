e2ba0da4416d910286aada19ea95eb0e2951a02d
Jamie Hardt <jamiehardt@me.com>
2018-01-30 22:46:21 -0800

    Elaborated the XML Document

--- PText Convert/XMLConversionEngine.swift
+++ PText Convert/XMLConversionEngine.swift
@@ -22,7 +22,10 @@ class XMLConversionEngine: NSObject {
         let records = tabulator.records
         
         let root = XMLElement(name: "pttext")
-        
+        root.addChild(XMLNode.comment(withStringValue: "Be advised this XML format is under active development and the schema may change at any time") as! XMLNode)
+        root.setAttributesAs(["testMode" : "true"])
+        root.addChild(XMLElement(name: "producer_identifer", stringValue: Bundle.main.bundleIdentifier))
+        root.addChild(XMLElement(name: "producer_version", stringValue: (Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String) ?? ""))
         let eventsEntity = XMLElement(name: "events")
         for record in records {
             let event = record.toXMLElement(named: "event")
