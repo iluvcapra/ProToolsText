eae366915b859762f2eb16cfab8c3ed8ce50f13d
Jamie Hardt <jamiehardt@me.com>
2018-09-27 23:02:22 -0700

    Implementation of XMLRepresentable

--- PText Convert/XML Report Entites/XMLStringDictionary.swift
+++ PText Convert/XML Report Entites/XMLStringDictionary.swift
@@ -22,29 +22,26 @@ extension String: XMLRepresentable {
     }
 }
 
-extension Dictionary where Key == String, Value == String {
+extension Dictionary where Key == String, Value : XMLRepresentable {
     
     func toXMLElement(named n : String) -> XMLElement {
         
         let retVal = XMLElement(name: n)
 
         for (key, value) in self {
-             if !value.isEmpty {
-                let memberElement = XMLElement(name: "field")
-                if key != value {
-                    let keyElement = XMLElement(name: "key", stringValue: key)
-                    let valueElement = XMLElement(name: "value")
-                    valueElement.addChild( value.xmlEntity() )
-                    
-                    memberElement.addChild(keyElement)
-                    memberElement.addChild(valueElement)
-                    
-                } else {
-                    let propertyElement = XMLElement(name: "property", stringValue: key)
-                    memberElement.addChild(propertyElement)
-                }
-                retVal.addChild(memberElement)
+            let memberElement : XMLElement = XMLElement(name: "field")
+            if let strValue = value as? String, strValue == key {
+                let propertyElement = XMLElement(name: "property", stringValue: key)
+                memberElement.addChild(propertyElement)
+                
+            } else {
+                let keyElement = XMLElement(name: "key", stringValue: key)
+                let valueElement = XMLElement(name: "value")
+                valueElement.addChild( value.xmlEntity() )
+                memberElement.addChild(keyElement)
+                memberElement.addChild(valueElement)
             }
+            retVal.addChild(memberElement)
         }
         
         return retVal
