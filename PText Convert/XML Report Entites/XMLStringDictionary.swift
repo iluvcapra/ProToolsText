96a94ac0eba81552a20cd1dfc8b29d4f5f53d4c5
Jamie Hardt <jamiehardt@me.com>
2018-09-27 21:21:29 -0700

    Rejiggering XML field value representation

--- PText Convert/XML Report Entites/XMLStringDictionary.swift
+++ PText Convert/XML Report Entites/XMLStringDictionary.swift
@@ -7,19 +7,34 @@
 
 import Foundation
 
+protocol XMLRepresentable {
+    
+    func xmlEntity() -> XMLNode
+    
+}
+
+extension String: XMLRepresentable {
+    
+    func xmlEntity() -> XMLNode {
+        let node = XMLNode(kind: XMLNode.Kind.text)
+        node.stringValue = self
+        return node
+    }
+}
+
 extension Dictionary where Key == String, Value == String {
     
     func toXMLElement(named n : String) -> XMLElement {
         
         let retVal = XMLElement(name: n)
 
-        
         for (key, value) in self {
              if !value.isEmpty {
                 let memberElement = XMLElement(name: "field")
                 if key != value {
                     let keyElement = XMLElement(name: "key", stringValue: key)
-                    let valueElement = XMLElement(name: "value", stringValue: value)
+                    let valueElement = XMLElement(name: "value")
+                    valueElement.addChild( value.xmlEntity() )
                     
                     memberElement.addChild(keyElement)
                     memberElement.addChild(valueElement)
